53d5bb6aeb58a08afa87a58f149bdf3365539989
avani898 <94886488+avani898@users.noreply.github.com>
2022-07-10 18:50:30 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RootNavigator from './app/navigator/RootNavigator';
+import { Provider } from 'react-redux'
+import store from './app/store/store';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
+}
